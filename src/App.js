2bf2063a4d9e5e59968708de6ed397655c771cb4
PaolaGuanca <paolarguanca@gmail.com>
2022-08-29 18:01:09 -0300

    se agrega Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+     <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Perfumática</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Nosotros</Nav.Link>
+            <Nav.Link href="#link">Marcas</Nav.Link>
+            <NavDropdown title="Categorías" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Mujer</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Hombre</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Niños</NavDropdown.Item>
+              <NavDropdown.Divider/>
+              <NavDropdown.Item href="#action/3.4">Unisex</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+     </Navbar>
+    </header>   
   );
 }
 
 export default App;
+
