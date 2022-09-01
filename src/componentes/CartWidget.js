caa33b9e6d4a981bebf18363139861cc8cc30e90
PaolaGuanca <paolarguanca@gmail.com>
2022-09-01 16:04:55 -0300

    Perfumática Tienda de perfumes-Guanca

---
+++ src/componentes/CartWidget.js
@@ -0,0 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import '../App.css';
+
+function CartWidget () {
+    return (
+      <div className="cart-widget">
+        
+        <FontAwesomeIcon icon={faCartShopping} size="1x" color="grey" />
+        <div className="qty-display">0</div>
+      </div>
+    );
+  };
+  
+  export default CartWidget;
+  
---
+++ src/componentes/ItemListContainer.js
@@ -0,0 +1,11 @@
+import '../App.css';
+
+function ItemListContainer() {
+    return (
+      <div className="list-container">   
+        <h1>Promociones</h1>
+      </div>
+    );
+  };
+
+  export default ItemListContainer;
