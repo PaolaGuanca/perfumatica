4a9b968a216c4db4b9b8ceb8e6c14187505fc19f
PaolaGuanca <paolarguanca@gmail.com>
2022-10-04 22:52:48 -0300

    se agrega collection

--- src/componentes/Cart.js
+++ src/componentes/Cart.js
@@ -1,9 +1,53 @@
 import { useContext } from "react"
 import { CartContext } from "./CartContext";
+import { collection, doc, setDoc, serverTimestamp, updateDoc, increment } from "firebase/firestore";
+import db from '../Datos/firebaseConfig';
 import '../App.css';
 
 const Cart = () => {
     const contx = useContext(CartContext);
+
+    const createOrder = () => {
+      const itemsForDB = test.cartList.map(item => ({
+        id: item.idItem,
+        title: item.nameItem,
+        price: item.costItem
+      }));
+  
+      test.cartList.forEach(async (item) => {
+        const itemRef = doc(db, "products", item.idItem);
+        await updateDoc(itemRef, {
+          stock: increment(-item.qtyItem)
+        });
+      });
+  
+      let order = {
+        buyer: {
+          name: "Pepe Sanchez",
+          email: "pepe@gmail.com",
+          phone: "555348963"
+        },
+        total: test.calcTotal(),
+        items: itemsForDB,
+        date: serverTimestamp()
+      };
+    
+      console.log(order);
+      
+      const createOrderInFirestore = async () => {
+        // Add a new document with a generated id
+        const newOrderRef = doc(collection(db, "orders"));
+        await setDoc(newOrderRef, order);
+        return newOrderRef;
+      }
+    
+      createOrderInFirestore()
+        .then(result => alert('Your order has been created. Please take note of the ID of your order.\n\n\nOrder ID: ' + result.id + '\n\n'))
+        .catch(err => console.log(err));
+    
+      test.removeList();
+    
+    }
 
     return (
         <>
@@ -34,7 +78,7 @@ const Cart = () => {
                             <li><h2>Resumen de la compra</h2></li>
                             <li>Cantidad de perfumes: {contx.calcItemsQty()}</li>
                             <li>Total: $ {contx.precioTotal()}</li>
-                            <li><button type="button" class="btn btn-secondary">Finalizar Compra</button></li>
+                            <li><button type="button" class="btn btn-secondary" onClick={createOrder}>Finalizar Compra</button></li>
                       </ul>
                 }
                 </div>
